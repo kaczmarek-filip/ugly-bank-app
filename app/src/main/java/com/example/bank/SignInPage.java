896303eab8ddead82431c61a670d554fe85e9939
kaczmarek-filip <fkaczmarekf@gmail.com>
2023-11-20 13:59:17 +0100

    SignInPage update

--- app/src/main/java/com/example/bank/SignInPage.java
+++ app/src/main/java/com/example/bank/SignInPage.java
@@ -1,5 +1,7 @@
 package com.example.bank;
 
+import static com.example.bank.ManageUser.users;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -29,21 +31,21 @@ public class SignInPage extends AppCompatActivity {
         buttonSignInSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /**
+                /*
                  * Kod pobiera wartości pól formularza
                  */
                 EditText textSignInLogin = (EditText) findViewById(R.id.textSignInLogin);
                 EditText textSignInPassword = (EditText) findViewById(R.id.textSignInPassword);
                 EditText textSignInPassword2 = (EditText) findViewById(R.id.textSignInPassword2);
 
-                /**
+                /*
                  * Wyciągnięcie wartości z obiektów
                  */
                 String login = textSignInLogin.getText().toString();
                 String password = textSignInPassword.getText().toString();
                 String password2 = textSignInPassword2.getText().toString();
 
-                /**
+                /*
                  * Sprawdzenie wymagań dot. haseł
                  */
                 if(password.length() < 8){
@@ -51,24 +53,32 @@ public class SignInPage extends AppCompatActivity {
                 } else if (!password.equals(password2)) {
                     Toast.makeText(SignInPage.this, "Hasła nie są sobie równe", Toast.LENGTH_SHORT).show();
                 } //TODO: dodać więcej warunków
-                //TODO: Sprawdzić czy użytkownik o podanym loginie nie jest już zarejestrowany
                 else {
-                    /**
+                    /*
                      * Jeśli hasło spełnia wymagania:
                      * 1. Wyświetl komunikat o poprawności danych
                      * 2. Prześlij login i hasło do strony logowania
                      * 3. Przejdź do strony logowania
                      * 4. Stwórz użytkownika z podanych danych
+                     * 5. Sprawdź czy login nie jest już używany
                      */
-                    Intent intent = new Intent(SignInPage.this, LoginPage.class);
-                    Toast.makeText(SignInPage.this, "Dane są poprawne", Toast.LENGTH_SHORT).show();
-                    intent.putExtra("login", login);
-                    intent.putExtra("password", password);
-                    startActivity(intent);
-                    User newUser = new User(login, password);
-                    ManageUser.addUser(newUser);
+                    for(User user : users){
+                        if(login.equals(user.getLogin())){
+                            Toast.makeText(SignInPage.this, "Użytkownik o podanym loginie już istnieje", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intent = new Intent(SignInPage.this, LoginPage.class);
+                            Toast.makeText(SignInPage.this, "Dane są poprawne", Toast.LENGTH_SHORT).show();
+                            intent.putExtra("login", login);
+                            intent.putExtra("password", password);
+                            startActivity(intent);
+                            User newUser = new User(login, password);
+                            ManageUser.addUser(newUser);
+                        }
+                    }
 
                 }
+
+
             }
         });
 
